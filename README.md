06b6a3c7f9b1ebc49ab38e75de3ef0919b6fb315
Tyler Frasca <tylerfrasca@Elevates-MacBook-Pro.local>
2016-09-14 23:49:05 -0700

    Init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Google Places Parser
+Simple script to grab Google Places results from input box and parse the results into a simpler format to grab each component (e.g. street, country, etc.).
---
+++ googlePlacesParser.js
@@ -0,0 +1,93 @@
+// Place class definition
+var Place = function() {
+  this.street_number = "";
+  this.street = "";
+  this.city = "";
+  this.state = "";
+  this.country = "";
+  this.zip_code = "";
+  this.full_address = "";
+  this.phone_number = "";
+  this.latitude = "";
+  this.longitude = "";
+  this.types = "";
+  this.vicinity = "";
+}
+
+//Google Places API
+function parseGooglePlace(id) {
+  var geocoder = new google.maps.Geocoder();
+  var address = document.getElementById(id).value;
+  var places = [];
+
+  geocoder.geocode({ 'address': address }, function (results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+
+      /*****TEMP???*****/
+      var resultsDiv = document.getElementById('results');
+      var inner = "";
+
+      console.log("RESULTS LENGTH");
+      console.log(results.length);
+
+      for(var resultIndex = 0; resultIndex < results.length; resultIndex++) {
+
+        // inner += "<p>" + resultIndex + "</p>";
+
+       var place = new Place();
+
+       place.latitude = results[resultIndex].geometry.location.lat;
+       place.longitude = results[resultIndex].geometry.location.lng;
+       place.full_address = results[resultIndex].formatted_address;
+       place.phone_number = results[resultIndex].formatted_phone_number;
+       place.types = results[resultIndex].types;
+       place.vicinity = results[resultIndex].vicinity;
+
+       for(i = 0; i < results[resultIndex].address_components.length; i++){
+         if (results[resultIndex].address_components[i].types[0] == "street_number") {
+           place.street_number = results[resultIndex].address_components[i].long_name;
+         }
+         else if (results[resultIndex].address_components[i].types[0] == "route") {
+           place.street = results[resultIndex].address_components[i].long_name;
+         }
+         else if (results[resultIndex].address_components[i].types[0] == "locality") {
+           place.city = results[resultIndex].address_components[i].long_name;
+         }
+         else if (results[resultIndex].address_components[i].types[0] == "administrative_area_level_1") {
+           place.state = results[resultIndex].address_components[i].long_name;
+         }
+         else if (results[resultIndex].address_components[i].types[0] == "country") {
+           place.country = results[resultIndex].address_components[i].long_name;
+         }
+         else if (results[resultIndex].address_components[i].types[0] == "postal_code") {
+           place.zip_code = results[resultIndex].address_components[i].long_name;
+         }
+       }
+
+       places.push(place);
+      }
+
+      //  resultsDiv.innerHTML = inner;
+    }
+  });
+  console.log("PLACES");
+  console.log(places);
+}
+
+function getGoogleLocation() {
+   new google.maps.places.Autocomplete(
+   (document.getElementById('places-input')), {
+       types: ['geocode']
+   });
+}
+
+getGoogleLocation();
+
+$(document).ready(function() {
+  $('#places-input').on('change', function() {
+    parseGooglePlace("places-input");
+  });
+  $('#places-input').on('click', function() {
+    parseGooglePlace("places-input");
+  });
+});
